8ccfab7449be0a7d6e7a1b9663034401a5843e74
Moisewastaken <moise.cochin@gmail.com>
2022-10-06 16:58:24 +0200

    Push .h

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: twinters <twinters@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mcochin <mcochin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 09:55:32 by twinters          #+#    #+#             */
-/*   Updated: 2022/10/06 11:19:05 by twinters         ###   ########.fr       */
+/*   Updated: 2022/10/06 16:07:29 by mcochin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,27 @@
 
 // --------------------------------- INCLUDES ----------------------------------
 
-// miscellaneous
 # include "libft/libft.h"
 # include <stdio.h>
-
-// waitpid
+# include <stdlib.h>
+# include <unistd.h>
 # include <sys/types.h>
 # include <sys/wait.h>
-
-// readline
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+# include <sys/ressource.h>
+# include <dirent.h>
+# include <errno.h>
 
 // --------------------------------- DEFINES -----------------------------------
 
 # define CHILD 0
+# define PIPE_OUT 0
+# define PIPE_IN 1
 
 // --------------------------------- STRUCTURES --------------------------------
 
